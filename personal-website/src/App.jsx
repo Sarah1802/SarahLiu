aea28987293d015318a24fd972552930852839a9
Sarah <Sarah.Liu.1802@gmail.com>
2024-12-26 21:53:34 -0800

    added offering & typewriting animation

---
+++ personal-website/src/App.jsx
@@ -0,0 +1,19 @@
+
+import Offering from './Offering'
+import './Offering.css'
+import AboutMe from './AboutMe'
+
+
+function App() {
+
+
+  return (
+    <>
+      <Offering/>
+      <AboutMe/>
+    </>
+    
+  )
+}
+
+export default App
---
+++ personal-website/src/Offering.jsx
@@ -0,0 +1,12 @@
+
+function Offering() {
+    return (
+        <div className="container">
+            <div className="animated-text">
+                Can I get you a  <span></span>
+            </div>
+         </div>
+    )
+}
+
+export default Offering
